188258b2ccc3623aa52be81e7d1f0d1975ed35f0
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-08-25 20:21:09 -0300

    adiciona calculadora cli

--- src/index.php
+++ src/index.php
@@ -4,4 +4,46 @@ require_once('vendor/autoload.php');
 
 $climate = new \League\CLImate\CLImate;
 
-$climate->out('Escrever no terminal.');
+$climate->out('[1] Somar');
+$climate->out('[2] Subtrair');
+$climate->out('[3] Multiplicar');
+$climate->out('[4] Dividir');
+
+$input = $climate->input('Escolha uma das opções acima: ');
+$input->accept(['1', '2', '3', '4']);
+$menu = $input->prompt();
+
+switch ($menu) {
+    case '1':
+        $numero1Input = $climate->input('Insira o primeiro número: ');
+        $numero1 = $numero1Input->prompt();
+        $numero2Input = $climate->input('Insira o segundo número: ');
+        $numero2 = $numero2Input->prompt();
+        echo sprintf('Resultado: %s', (int) $numero1 + (int) $numero2);
+        break;
+    case '2':
+        $numero1Input = $climate->input('Insira o primeiro número: ');
+        $numero1 = $numero1Input->prompt();
+        $numero2Input = $climate->input('Insira o segundo número: ');
+        $numero2 = $numero2Input->prompt();
+        echo sprintf('Resultado: %s', (int) $numero1 - (int) $numero2);
+        break;
+    case '3':
+        $numero1Input = $climate->input('Insira o primeiro número: ');
+        $numero1 = $numero1Input->prompt();
+        $numero2Input = $climate->input('Insira o segundo número: ');
+        $numero2 = $numero2Input->prompt();
+        echo sprintf('Resultado: %s', (int) $numero1 * (int) $numero2);
+        break;
+    case '4':
+        $numero1Input = $climate->input('Insira o primeiro número: ');
+        $numero1 = $numero1Input->prompt();
+        $numero2Input = $climate->input('Insira o segundo número: ');
+        $numero2 = $numero2Input->prompt();
+        echo sprintf('Resultado: %s', (int) $numero1 / (int) $numero2);
+        break;
+
+    default:
+        # code...
+        break;
+}
