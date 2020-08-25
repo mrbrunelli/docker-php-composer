3081d865cf215536cd1f78fcfce9d779287c5a48
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-08-25 20:34:35 -0300

    refatora calculadora

--- src/index.php
+++ src/index.php
@@ -2,7 +2,9 @@
 
 require_once('vendor/autoload.php');
 
-$climate = new \League\CLImate\CLImate;
+use League\CLImate\CLImate;
+
+$climate = new CLImate;
 
 $climate->out('[1] Somar');
 $climate->out('[2] Subtrair');
@@ -13,34 +15,42 @@ $input = $climate->input('Escolha uma das opções acima: ');
 $input->accept(['1', '2', '3', '4']);
 $menu = $input->prompt();
 
+class Operacao
+{
+    public $numero1;
+    public $numero2;
+    protected $climate;
+
+    public function __construct(CLImate $climate)
+    {
+        $this->climate = $climate;
+    }
+
+    public function input()
+    {
+        $numero1Input = $this->climate->input('Insira o primeiro número: ');
+        $this->numero1 = $numero1Input->prompt();
+        $numero2Input = $this->climate->input('Insira o segundo número: ');
+        $this->numero2 = $numero2Input->prompt();
+    }
+}
+
+$operacao = new Operacao($climate);
+$operacao->input();
+
 switch ($menu) {
     case '1':
-        $numero1Input = $climate->input('Insira o primeiro número: ');
-        $numero1 = $numero1Input->prompt();
-        $numero2Input = $climate->input('Insira o segundo número: ');
-        $numero2 = $numero2Input->prompt();
-        echo sprintf('Resultado: %s', (int) $numero1 + (int) $numero2);
+
+        echo sprintf('Resultado: %s', (int) $operacao->numero1 + (int) $operacao->numero2);
         break;
     case '2':
-        $numero1Input = $climate->input('Insira o primeiro número: ');
-        $numero1 = $numero1Input->prompt();
-        $numero2Input = $climate->input('Insira o segundo número: ');
-        $numero2 = $numero2Input->prompt();
-        echo sprintf('Resultado: %s', (int) $numero1 - (int) $numero2);
+        echo sprintf('Resultado: %s', (int) $operacao->numero1 - (int) $operacao->numero2);
         break;
     case '3':
-        $numero1Input = $climate->input('Insira o primeiro número: ');
-        $numero1 = $numero1Input->prompt();
-        $numero2Input = $climate->input('Insira o segundo número: ');
-        $numero2 = $numero2Input->prompt();
-        echo sprintf('Resultado: %s', (int) $numero1 * (int) $numero2);
+        echo sprintf('Resultado: %s', (int) $operacao->numero1 * (int) $operacao->numero2);
         break;
     case '4':
-        $numero1Input = $climate->input('Insira o primeiro número: ');
-        $numero1 = $numero1Input->prompt();
-        $numero2Input = $climate->input('Insira o segundo número: ');
-        $numero2 = $numero2Input->prompt();
-        echo sprintf('Resultado: %s', (int) $numero1 / (int) $numero2);
+        echo sprintf('Resultado: %s', (int) $operacao->numero1 / (int) $operacao->numero2);
         break;
 
     default:
